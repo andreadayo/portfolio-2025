9eaab3b4ba49bd4e50c0d4933e244436ce06cc26
Andrea Dayo <72564321+andreadayo@users.noreply.github.com>
2025-10-06 01:59:46 +0800

    feat(about-approach): create approach section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
 import { PaddingWrapper } from "@components/PaddingWrapper/styles";
 import Introduction from "@/src/components/About/Introduction";
+import Approach from "@/src/components/About/Approach";
 
 export default function Home() {
   return (
     <>
       <PaddingWrapper>
         <Introduction />
+        <Approach />
       </PaddingWrapper>
     </>
   );
